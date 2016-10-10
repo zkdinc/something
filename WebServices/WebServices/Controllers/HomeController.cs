bc4de1534389efac79a6a4112f30a30b659dce23
Zaid Khaled <Zaid Khaled@ZAIDKHALED-TOSH>
2016-10-10 19:54:30 +0300

    hola

--- WebServices/WebServices/Controllers/HomeController.cs
+++ WebServices/WebServices/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace WebServices.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            ViewBag.Title = "Home Pages";
 
             return View();
         }
